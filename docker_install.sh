4a8114edbe42091bcb19b1a2a598867ba5db5045
nanangqq <gingggg@gmail.com>
2020-02-25 22:54:47 +0000

    0225

--- docker_install.sh
+++ docker_install.sh
@@ -1,3 +1,4 @@
+sudo apt-get update
 sudo sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
