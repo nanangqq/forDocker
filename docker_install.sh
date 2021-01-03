1e4456d0e5c1d4b8395a0e5145bf829281403631
nanangqq <gingggg@gmail.com>
2021-01-03 11:33:26 +0000

    arm

--- docker_install.sh
+++ docker_install.sh
@@ -1,7 +1,7 @@
-sudo apt-get update
-sudo sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-sudo usermod -aG docker $USER
+add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu focal stable"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io -y
+usermod -aG docker ging
